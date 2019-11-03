37d46455f7c51fd7143e3e6bd079f7ffa25609c8
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-03 15:16:14 +0700

    Second Commit. Not working yet

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from pymongo import MongoClient 
 
-client = MongoClient("mongodb+srv://Raymondo:<password>@cluster0-jotax.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://Raymondo:M0ng0DB2019@cluster0-jotax.mongodb.net/test?retryWrites=true&w=majority")
+
+db = client.get_database('Ramones')
+
+records = db.Albums
+
+#records.count_documents({})
+list(records.find())
+
+
+
