f8f64be8358383d3c2df372a7aa629b455a08a9c
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-03 16:18:21 +0700

    Third example is connecting and working

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from pymongo import MongoClient 
 
 client = MongoClient("mongodb+srv://Raymondo:M0ng0DB2019@cluster0-jotax.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
 
 db = client.get_database('Ramones')
 
-records = db.Albums
+dblist = client.list_database_names()
+if "Ramones" in dblist:
+  print("The database exists.")
 
-#records.count_documents({})
-list(records.find())
+mycol = db["Albums"]
+x = mycol.find_one()
+print(x)
+
+print("The collection name is : ", db.list_collection_names())
+
+records = db.Albums
+list(db.Albums.find())
 
 
 
